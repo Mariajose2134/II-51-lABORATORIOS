4b65a0d2bd554e4620af6a846bef5c9cb1591772
María José FALLAS <mariajosefallascr1516@gmail.com>
2025-11-18 14:12:10 -0600

    Renombra la clase de CSS de .tema-oscuro a .oscuro y simplifica la función alternarTema para cambiar el tema claro/oscuro.

--- LAB-07/script.js
+++ LAB-07/script.js
@@ -36,14 +36,6 @@ $("#mostrarOcultar").click(function () {
   };
 
   // Alternar tema claro/oscuro
-let temaOscuro = false;
 function alternarTema() {
-  temaOscuro = !temaOscuro;
-  if (temaOscuro) {
-    document.getElementById("tema").className = "fa-solid fa-sun";
-  } else {
-    document.getElementById("tema").className = "fa-solid fa-moon";
+    document.getElementById("contenido").classList.toggle("oscuro");
   }
- 
-  document.getElementById("contenido").classList.toggle("oscuro");
-}
